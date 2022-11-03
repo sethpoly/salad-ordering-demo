663974b48c9e0df04390aa4ad6372b36c35849bf
Seth Polyniak <emailspolyniak@gmail.com>
2022-11-03 08:58:18 -0400

    [feat/#2] - add color assets + ext

---
+++ Shared/Resource/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorManager.swift
+//  SaladFoodOrderingApp (iOS)
+//
+//  Created by Seth Polyniak on 11/3/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension Color {
+    static let background = Color("background")
+    static let onBackground = Color("onBackground")
+    static let primary = Color("primary")
+    static let onPrimary = Color("onPrimary")
+    static let secondaryColor = Color("secondary")
+    static let onSecondary = Color("onSecondary")
+}
