a7548419b10db826bdda7d1f0df3fc5f02a53020
Seth Polyniak <emailspolyniak@gmail.com>
2022-11-06 09:30:29 -0500

    [feat/#7] - add outline for all elements

---
+++ Shared/Resource/ShapeManager.swift
@@ -0,0 +1,14 @@
+//
+//  ShapeManager.swift
+//  SaladFoodOrderingApp (iOS)
+//
+//  Created by Seth Polyniak on 11/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+class ShapeManager {
+    static let cardShape: CGFloat = 12
+    static let buttonShape: CGFloat = 8
+}
--- Shared/SaladFoodOrderingAppApp.swift
+++ Shared/SaladFoodOrderingAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SaladFoodOrderingAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ItemView()
         }
     }
 }
