8e91bc1c11fb6169c9f02a791702f565a9926d79
Seth Polyniak <emailspolyniak@gmail.com>
2022-11-26 09:05:45 -0500

    [feat/images] - auto-reformat indentation

--- Shared/Component/Tappable.swift
+++ Shared/Component/Tappable.swift
@@ -7,12 +7,13 @@ struct Tappable: ViewModifier {
     func body(content: Content) -> some View {
         content
             .scaleEffect(tap ? 1.05 : 1)
-            .animation(.spring(response: 0.4, dampingFraction: 1))
             .contentShape(Rectangle())
             .onTapGesture {
                 tap = true
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                    tap = false
+                    withAnimation(.spring(response: 0.4, dampingFraction: 1)) {
+                        tap = false
+                    }
                 }
                 onTap()
             }
